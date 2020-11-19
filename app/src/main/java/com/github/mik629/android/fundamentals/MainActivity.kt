169ad2652bdffb4d3ef104a12abb8771d4f2c5d7
Mikhail <mikhail.id.123@gmail.com>
2020-11-19 09:51:05 +0400

    Implement MovieDetails screen according to design

--- app/src/main/java/com/github/mik629/android/fundamentals/MainActivity.kt
+++ app/src/main/java/com/github/mik629/android/fundamentals/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.github.mik629.android.fundamentals
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mik629.android.fundamentals.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.movieDetailsBtn.setOnClickListener { navigateToMovieDetailsScreen() }
+    }
+
+    private fun navigateToMovieDetailsScreen() {
+        startActivity(
+            MovieDetailsActivity.createIntent(this)
+        )
     }
 }
