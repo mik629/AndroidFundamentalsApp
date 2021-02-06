5caf2cd174b93ef20367a7dd1e38fae120cf59d0
Mikhail <mikhail.id.123@gmail.com>
2021-02-06 20:34:38 +0400

    Home work 8
    * use viewModel scope that handles cancellation

--- app/src/main/java/com/github/mik629/android/fundamentals/ui/movieslist/MoviesListViewModel.kt
+++ app/src/main/java/com/github/mik629/android/fundamentals/ui/movieslist/MoviesListViewModel.kt
@@ -3,24 +3,22 @@ package com.github.mik629.android.fundamentals.ui.movieslist
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.github.mik629.android.fundamentals.domain.model.Movie
 import com.github.mik629.android.fundamentals.domain.repositories.MoviesRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MoviesListViewModel(
     private val moviesRepository: MoviesRepository
 ) : ViewModel() {
 
-    private val scope = CoroutineScope(Dispatchers.Main)
     private val _movies: MutableLiveData<List<Movie>> = MutableLiveData()
     val movies: LiveData<List<Movie>>
         get() =
             _movies
 
     init {
-        scope.launch {
+        viewModelScope.launch {
             _movies.value = moviesRepository.getMovies()
         }
     }
