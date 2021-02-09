88edbb4e04d31fd3d2e12ccb02be6b4cf8940c6c
Mikhail <mikhail.id.123@gmail.com>
2021-02-09 16:48:20 +0400

    Home work 8
    * optimize complex inserts

--- app/src/main/java/com/github/mik629/android/fundamentals/data/db/models/MovieActorCrossRef.kt
+++ app/src/main/java/com/github/mik629/android/fundamentals/data/db/models/MovieActorCrossRef.kt
@@ -1,25 +1,11 @@
 package com.github.mik629.android.fundamentals.data.db.models
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
-import androidx.room.PrimaryKey
 import com.github.mik629.android.fundamentals.data.db.models.ActorDbEntity.Companion.COLUMN_NAME_ACTOR_ID
 import com.github.mik629.android.fundamentals.data.db.models.MovieDbEntity.Companion.COLUMN_NAME_MOVIE_ID
 
-@Entity
+@Entity(primaryKeys = [COLUMN_NAME_MOVIE_ID, COLUMN_NAME_ACTOR_ID])
 data class MovieActorCrossRef(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
-    @ForeignKey(
-        entity = MovieDbEntity::class,
-        parentColumns = [COLUMN_NAME_MOVIE_ID],
-        childColumns = [COLUMN_NAME_MOVIE_ID]
-    )
     val movieId: String,
-    @ForeignKey(
-        entity = ActorDbEntity::class,
-        parentColumns = [COLUMN_NAME_ACTOR_ID],
-        childColumns = [COLUMN_NAME_ACTOR_ID]
-    )
     val actorId: String
 )
--- app/src/main/java/com/github/mik629/android/fundamentals/data/db/models/MovieGenreCrossRef.kt
+++ app/src/main/java/com/github/mik629/android/fundamentals/data/db/models/MovieGenreCrossRef.kt
@@ -1,25 +1,11 @@
 package com.github.mik629.android.fundamentals.data.db.models
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
-import androidx.room.PrimaryKey
 import com.github.mik629.android.fundamentals.data.db.models.GenreDbEntity.Companion.COLUMN_NAME_GENRE_ID
 import com.github.mik629.android.fundamentals.data.db.models.MovieDbEntity.Companion.COLUMN_NAME_MOVIE_ID
 
-@Entity
+@Entity(primaryKeys = [COLUMN_NAME_MOVIE_ID, COLUMN_NAME_GENRE_ID])
 data class MovieGenreCrossRef(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
-    @ForeignKey(
-        entity = MovieDbEntity::class,
-        parentColumns = [COLUMN_NAME_MOVIE_ID],
-        childColumns = [COLUMN_NAME_MOVIE_ID]
-    )
     val movieId: String,
-    @ForeignKey(
-        entity = GenreDbEntity::class,
-        parentColumns = [COLUMN_NAME_GENRE_ID],
-        childColumns = [COLUMN_NAME_GENRE_ID]
-    )
     val genreId: String
 )
