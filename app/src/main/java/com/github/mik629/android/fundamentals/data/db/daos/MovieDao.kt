c7e0ddd2b5a8c37c33e46af98e2873fa2d4d4bd6
Mikhail <mikhail.id.123@gmail.com>
2021-02-10 10:39:00 +0400

    Home work 8
    * hide non-public dao methods

--- app/src/main/java/com/github/mik629/android/fundamentals/data/db/daos/MovieDao.kt
+++ app/src/main/java/com/github/mik629/android/fundamentals/data/db/daos/MovieDao.kt
@@ -16,36 +16,36 @@ import com.github.mik629.android.fundamentals.data.db.models.MovieGenreCrossRef
 import com.github.mik629.android.fundamentals.data.db.models.MovieWithActorsAndGenres
 
 @Dao
-interface MovieDao {
+abstract class MovieDao {
     @Transaction
     @Query(value = "SELECT * FROM $MOVIES_TABLE_NAME ORDER BY $COLUMN_NAME_RATING DESC")
-    suspend fun getAllMovies(): List<MovieWithActorsAndGenres>
+    abstract suspend fun getAllMovies(): List<MovieWithActorsAndGenres>
 
     @Query(value = "SELECT * FROM $MOVIES_TABLE_NAME WHERE $COLUMN_NAME_MOVIE_ID = :id")
-    suspend fun getMovie(id: Long): MovieWithActorsAndGenres?
+    abstract suspend fun getMovie(id: Long): MovieWithActorsAndGenres?
 
     @Transaction
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMovies(movies: List<MovieDbEntity>)
+    protected abstract suspend fun insertMovies(movies: List<MovieDbEntity>)
 
     @Transaction
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertActors(actors: List<ActorDbEntity>)
+    protected abstract suspend fun insertActors(actors: List<ActorDbEntity>)
 
     @Transaction
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMovieActors(movieActors: List<MovieActorCrossRef>)
+    protected abstract suspend fun insertMovieActors(movieActors: List<MovieActorCrossRef>)
 
     @Transaction
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertGenres(genres: List<GenreDbEntity>)
+    protected abstract suspend fun insertGenres(genres: List<GenreDbEntity>)
 
     @Transaction
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMovieGenres(movieGenres: List<MovieGenreCrossRef>)
+    protected abstract suspend fun insertMovieGenres(movieGenres: List<MovieGenreCrossRef>)
 
     @Transaction
-    suspend fun insertData(
+    open suspend fun insertData(
         movies: List<MovieDbEntity>,
         actors: List<ActorDbEntity>,
         movieActors: List<MovieActorCrossRef>,
