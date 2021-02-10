e5a190f8bc2d9506b1eb02d3e7d41880b74ce0f5
Mikhail <mikhail.id.123@gmail.com>
2021-02-10 06:10:11 +0400

    Home work 8
    * explicitly define return type for public routines

--- app/src/main/java/com/github/mik629/android/fundamentals/di/AppModule.kt
+++ app/src/main/java/com/github/mik629/android/fundamentals/di/AppModule.kt
@@ -1,10 +1,12 @@
 package com.github.mik629.android.fundamentals.di
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.github.mik629.android.fundamentals.BuildConfig
 import com.github.mik629.android.fundamentals.GlideApp
+import com.github.mik629.android.fundamentals.GlideRequest
 import com.github.mik629.android.fundamentals.R
 import com.github.mik629.android.fundamentals.data.db.MovieDb
 import com.github.mik629.android.fundamentals.data.network.ServerApi
@@ -73,7 +75,7 @@ class AppModule private constructor() {
     }
 }
 
-fun buildGlideRequest(fragment: Fragment) =
+fun buildGlideRequest(fragment: Fragment): GlideRequest<Drawable> =
     GlideApp.with(fragment)
         .asDrawable()
         .thumbnail(0.1f)
