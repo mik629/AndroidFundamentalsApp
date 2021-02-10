8277141d8a8219e5f46f13c3d2a34887e6f17b14
Mikhail <mikhail.id.123@gmail.com>
2021-02-10 21:54:01 +0400

    Home work 8
    * remove extra layout for rating_bar

--- app/src/main/java/com/github/mik629/android/fundamentals/ui/utils/Utils.kt
+++ app/src/main/java/com/github/mik629/android/fundamentals/ui/utils/Utils.kt
@@ -5,22 +5,9 @@ import androidx.core.content.ContextCompat
 import com.github.mik629.android.fundamentals.R
 import com.github.mik629.android.fundamentals.databinding.RatingBarBinding
 import com.github.mik629.android.fundamentals.databinding.RatingBarItemBinding
-import com.github.mik629.android.fundamentals.databinding.RatingBarSmallBinding
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun RatingBarBinding.setRating(context: Context, value: Float) {
-    val grey = ContextCompat.getColor(context, R.color.grey)
-    with(this) {
-        starFive.setGreyIf(grey, value, 5)
-        starFour.setGreyIf(grey, value, 4)
-        starThree.setGreyIf(grey, value, 3)
-        starTwo.setGreyIf(grey, value, 2)
-        starOne.setGreyIf(grey, value, 1)
-    }
-}
-
-@Suppress("NOTHING_TO_INLINE")
-inline fun RatingBarSmallBinding.setRating(context: Context, value: Float) {
     val grey = ContextCompat.getColor(context, R.color.grey)
     with(this) {
         arrayOf(starOne, starTwo, starThree, starFour, starFive)
