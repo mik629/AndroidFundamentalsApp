f830a10c7f49948e9fde59f7d1f914291c106a9c
Mikhail <mikhail.id.123@gmail.com>
2021-03-18 13:21:18 +0400

    Make API requests in parallel

--- app/src/main/java/com/github/mik629/android/fundamentals/data/MoviesLoader.kt
+++ app/src/main/java/com/github/mik629/android/fundamentals/data/MoviesLoader.kt
@@ -8,21 +8,28 @@ import com.github.mik629.android.fundamentals.data.network.model.ActorDTO
 import com.github.mik629.android.fundamentals.data.network.model.toActor
 import com.github.mik629.android.fundamentals.data.network.model.toMovie
 import com.github.mik629.android.fundamentals.domain.model.Movie
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
 import timber.log.Timber
 
 class MoviesLoader constructor(
     private val serverApi: ServerApi,
     private val dao: MovieDao
 ) {
+    private val scope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
     suspend fun loadMoviesFromCache(): List<Movie> =
         dao.getAllMovies()
             .map(MovieWithActorsAndGenres::toMovie)
 
     suspend fun loadMoviesFromNetwork(category: String = "popular"): List<Movie> {
         Timber.d("Loading from the network")
-        return serverApi.getMovieList(category)
+        return scope.async { serverApi.getMovieList(category) }
+            .await()
             .results
-            .map { movie -> loadMovieFromNetwork(movie.id) }
+            .map { movie -> scope.async { loadMovieFromNetwork(movie.id) }.await() }
             .sortedByDescending { movie -> movie.rating }
     }
 
@@ -34,10 +41,14 @@ class MoviesLoader constructor(
             ?.toMovie()
 
     private suspend fun loadMovieFromNetwork(id: Long): Movie {
-        val actors = serverApi.getMovieActors(id)
-            .cast
-            .map(ActorDTO::toActor)
-        return serverApi.getMovie(id)
+        val actors = scope.async {
+            serverApi.getMovieActors(id)
+                .cast
+                .map(ActorDTO::toActor)
+        }.await()
+
+        return scope.async { serverApi.getMovie(id) }
+            .await()
             .toMovie(actors)
     }
 }
